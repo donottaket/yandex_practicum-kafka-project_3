96947bd6bfb2e402f7a0d988fd673bea0e4987b0
maenlest <donottaket@gmail.com>
2025-08-07 00:14:15 +0500

    Refactor Docker Compose configuration and update initialization scripts

--- postgres/init.sql
+++ postgres/init.sql
@@ -13,13 +13,11 @@ CREATE TABLE orders (
     order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Добавление пользователей
 INSERT INTO users (name, email) VALUES ('John Doe', 'john@example.com');
 INSERT INTO users (name, email) VALUES ('Jane Smith', 'jane@example.com');
 INSERT INTO users (name, email) VALUES ('Alice Johnson', 'alice@example.com');
 INSERT INTO users (name, email) VALUES ('Bob Brown', 'bob@example.com');
 
--- Добавление заказов
 INSERT INTO orders (user_id, product_name, quantity) VALUES (1, 'Product A', 2);
 INSERT INTO orders (user_id, product_name, quantity) VALUES (1, 'Product B', 1);
 INSERT INTO orders (user_id, product_name, quantity) VALUES (2, 'Product C', 5);
